49db64417fb02dc08009664030ae6518e053f551
joshjal <JoshuaALevine3@gmail.com>
2025-04-04 17:50:48 -0400

    add react-compiler

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx', 'md']
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx', 'md'],
+  experimental: {
+    reactCompiler: true
+  }
 };
 
 const withMDX = createMDX({});
