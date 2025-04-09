6b1b2506093372079589ad85f5ae132b4b84f922
joshjal <JoshuaALevine3@gmail.com>
2025-04-09 17:37:52 -0400

    adjust back to top button location

--- src/components/ui/BackToTopButton.tsx
+++ src/components/ui/BackToTopButton.tsx
@@ -2,7 +2,7 @@ import ButtonLink from '@/components/ui/ButtonLink';
 
 export default function BackToTopButton() {
   return (
-    <ButtonLink additionalClasses='w-fit fixed bottom-3 z-50 right-5 sm:right-96' href='#top'>
+    <ButtonLink additionalClasses='w-fit fixed bottom-3 z-50 right-5 sm:right-84 md:right0-96' href='#top'>
       Back To Top
     </ButtonLink>
   );
