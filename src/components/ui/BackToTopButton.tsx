61bb858f3b0d8b07b0ffc459de544a7657da9376
joshjal <JoshuaALevine3@gmail.com>
2025-06-11 00:10:50 -0400

    make back to top blueish again

--- src/components/ui/BackToTopButton.tsx
+++ src/components/ui/BackToTopButton.tsx
@@ -3,7 +3,6 @@ import GlassButtonLink from '@/components/ui/GlassButtonLink';
 export default function BackToTopButton() {
   return (
     <GlassButtonLink
-      colorSwap4
       additionalClasses='w-fit fixed bottom-3 z-50 right-5 sm:right-84 md:right0-96 backdrop-blur-sm'
       href='#top'
     >
